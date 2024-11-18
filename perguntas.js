f55ad1fb3788c140f56475b99e2ada2cad946b8d
Pacoca209 <00001082048653sp@al.educacao.sp.gov.br>
2024-11-18 14:22:32 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geral',
+    'Por que o céu é azul?',
+    ' O céu parece azul porque as moléculas na atmosfera espalham a luz do sol em todas as direções. A luz azul tem um comprimento de onda mais curto e é espalhada mais do que outras cores, dando ao céu sua tonalidade característica.'
+)
+
+criaCartao(
+    'Geral',
+    'Por que os gatos ronronam?',
+    'Os gatos ronronam principalmente para expressar conforto e felicidade, mas também podem ronronar em situações de estresse, como quando estão com dor ou doentes.'
+)
+
+criaCartao(
+    'Geral',
+    'Qual é o lugar mais profundo do oceano?',
+    'O ponto mais profundo conhecido é a Fossa das Marianas, localizada no Oceano Pacífico. O local mais fundo dela é o Challenger Deep, que atinge aproximadamente 11.000 metros abaixo da superfície.'
+)
+
+criaCartao(
+    'Geral',
+    'É verdade que as árvores "falam" umas com as outras?',
+    'Sim, de certa forma! Árvores se comunicam através de um "Wood Wide Web", um sistema subterrâneo de fungos micorrízicos.'
+)
